c360f0cb53a214551d81d2df99207b25bfe7f63c
Jeferson Miranda G <jeferson.miranda@pucp.edu.pe>
2023-01-16 19:07:11 -0500

    amounts and taxes calculated

--- controllers/OrderItemController.js
+++ controllers/OrderItemController.js
@@ -1,5 +1,6 @@
 import OrderItemModel from '../models/OrderItemModel.js';
-import Product from '../models/ProductModel.js'
+import Product from '../models/ProductModel.js';
+import OrderModel from '../models/OrderModel.js';
 
 //show all order items
 export const getAllOrderItems = async (req, res) => {
@@ -16,7 +17,7 @@ export const getAllOrderItems = async (req, res) => {
 }
 
 //show all order items of one orden
-export const getAllOrderItemsByOrden = async (req, res) => {
+export const getAllOrderItemsByOrder = async (req, res) => {
   try {
     const orderItems = await OrderItemModel.findAll({
       include: [{
@@ -55,6 +56,33 @@ export const updateOrderItem = async (req, res) => {
   try {
     await OrderItemModel.update(req.body, { where: { idOrderItem: req.params.id } });
     res.json({ 'message': 'Order item has been updated successfully!.'});
+  } catch (e) {
+    res.json({ message: e.message })
+  }
+}
+
+//update amounts
+export const updateAmounts = async (req, res) => {
+  try {
+    const product = await Product.findAll({ where: { idProduct: req.body.idProduct } });
+    const newSubtotal = product[0].unitPrice * req.body.quantity;
+    const newCityTax = newSubtotal  * 0.1;
+    const newCountryTax = (newSubtotal + newCityTax) * 0.05;
+    const newStateTax = (newSubtotal + newCityTax + newCountryTax) * 0.08;
+    const newFederalTax = (newSubtotal + newCityTax + newCountryTax + newStateTax) * 0.02;
+    const newTotalTaxes = newCityTax + newCountryTax + newStateTax + newFederalTax;
+    const newTotalAmount = newSubtotal + newTotalTaxes;
+    const obj = {
+      subtotal: req.body.action === 'Add' ? req.body.subtotal + newSubtotal : (req.body.subtotal - newSubtotal),
+      cityTax: req.body.action === 'Add' ? req.body.cityTax + newCityTax : (req.body.cityTax - newCityTax),
+      countryTax: req.body.action === 'Add' ? req.body.countryTax + newCountryTax : (req.body.countryTax - newCountryTax),
+      stateTax: req.body.action === 'Add' ? req.body.stateTax + newStateTax : (req.body.stateTax - newStateTax),
+      federalTax: req.body.action === 'Add' ? req.body.federalTax + newFederalTax : (req.body.federalTax - newFederalTax),
+      totalTaxes: req.body.action === 'Add' ? req.body.totalTaxes + newTotalTaxes : (req.body.totalTaxes - newTotalTaxes),
+      totalAmount: req.body.action === 'Add' ? req.body.totalAmount + newTotalAmount : (req.body.totalAmount - newTotalAmount),
+    }
+    await OrderModel.update(obj, { where: { idOrder: req.body.idOrder } });
+    res.json({ 'message': 'Order has been updated successfully!.'});
   } catch (e) {
     res.json({ message: e.message })
   }
--- routes/OrderItemRoutes.js
+++ routes/OrderItemRoutes.js
@@ -1,13 +1,14 @@
 import express from 'express';
-import { getAllOrderItems, getAllOrderItemsByOrden, getOrderItem, createOrderItem, updateOrderItem, deleteOrderItem } from '../controllers/OrderItemController.js';
+import { getAllOrderItems, getAllOrderItemsByOrder, getOrderItem, createOrderItem, updateOrderItem, updateAmounts, deleteOrderItem } from '../controllers/OrderItemController.js';
 const router = express.Router();
 
 //order item routes
 router.get('/', getAllOrderItems);
-router.get('/:id', getAllOrderItemsByOrden);
+router.get('/:id', getAllOrderItemsByOrder);
 router.get('/:id', getOrderItem);
 router.post('/', createOrderItem);
 router.put('/:id', updateOrderItem);
+router.post('/amounts', updateAmounts);
 router.delete('/:id', deleteOrderItem);
 
 export default router;
